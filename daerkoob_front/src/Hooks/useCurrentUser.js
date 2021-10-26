d82f35e0fe7367cb12d4fa3c41f78472a21026b8
jaeyeon kim <kpeel5839@github.com>
2021-10-26 16:50:25 +0900

    remove front

--- daerkoob_front/src/Hooks/useCurrentUser.js
+++
@@ -1,9 +0,0 @@
-import { useRecoilState } from "recoil";
-import { currentUserState } from "Store";
-const useCurrentUser = () => {
-  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
-
-  return { currentUser, setCurrentUser };
-};
-
-export default useCurrentUser;
