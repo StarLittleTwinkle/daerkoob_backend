9d5f2b3044c1646f564e2ddec58b541cbf0eee5d
jaeyeon kim <jakind@naver.com>
2021-11-13 08:10:02 +0900

    click ,register 함수 추가 및 , Transcription create Dto , getTranscription 함수 추가

--- daerkoob_back/src/main/java/com/project/daerkoob/controller/TranscriptionController.java
+++ daerkoob_back/src/main/java/com/project/daerkoob/controller/TranscriptionController.java
@@ -1,18 +1,42 @@
 package com.project.daerkoob.controller;
 
+import com.project.daerkoob.domain.Transcription;
+import com.project.daerkoob.service.BookService;
 import com.project.daerkoob.service.TranscriptionService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.project.daerkoob.domain.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("transcription")
 public class TranscriptionController {
 
-    private TranscriptionService transcriptionService; 
+    private TranscriptionService transcriptionService;
+    private BookService bookService;
 
     public TranscriptionController(TranscriptionService transcriptionService){
         this.transcriptionService = transcriptionService;
+    }
+    @PostMapping("click") //책을 눌렀을 때 없으면 그냥 아무일도 안 일어남
+    public List<Transcription> click(Book book){
+        List<Transcription> transcriptions = new ArrayList<Transcription>();
+        if (bookService.existsBook(book)) {
+            transcriptions = transcriptionService.getTranscription(bookService.getBookId(book));
+        }
+        return transcriptions;
+    }
+
+
+    @PostMapping("register") //책에 대한 필사 내용을 적고 submit을 눌렀을 때
+    public void register(Long userId, Book book , String transcriptionContent){
+        bookService.save(book);
+        Transcription transcription = transcriptionService.createDto(userId, book.getId(), transcriptionContent);
+        transcriptionService.save(transcription);
     }
 }
