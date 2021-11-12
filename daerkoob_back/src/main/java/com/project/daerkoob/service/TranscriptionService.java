b0fdf1792c4ec3b4f01e49d2e0856e3a3265b664
jaeyeon kim <jakind@naver.com>
2021-11-13 08:09:03 +0900

    click , register 함수 추가

--- daerkoob_back/src/main/java/com/project/daerkoob/service/TranscriptionService.java
+++ daerkoob_back/src/main/java/com/project/daerkoob/service/TranscriptionService.java
@@ -1,13 +1,35 @@
 package com.project.daerkoob.service;
 
+import com.project.daerkoob.domain.Transcription;
 import com.project.daerkoob.repository.TranscriptionRepository;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
+import java.util.List;
 @Service
 public class TranscriptionService {
     private TranscriptionRepository transcriptionRepository;
 
     public TranscriptionService(TranscriptionRepository transcriptionRepository){
         this.transcriptionRepository = transcriptionRepository;
+    }
+
+    public List<Transcription> getTranscription(Long bookId){
+        List<Transcription> transcriptions = transcriptionRepository.findByBookId(bookId);
+        return transcriptions;
+    }
+
+    public Transcription createDto(Long userId , Long bookId , String transcriptionContent){
+        Transcription transcription = new Transcription();
+        transcription.setUserId(userId);
+        transcription.setBookId(bookId);
+        transcription.setStar(0D);
+        transcription.setThumb(0L);
+        transcription.setTranscription(transcriptionContent);
+        return transcription;
+    }
+
+    public void save(Transcription transcription){
+        transcriptionRepository.save(transcription);
     }
 }
