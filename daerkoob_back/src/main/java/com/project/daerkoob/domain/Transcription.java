160cc0d4b9a7aa7bfea8962789e5dd04af95f227
jaeyeon kim <kpeel5839@github.com>
2021-11-12 20:11:17 +0900

    transcription dto 추가

--- daerkoob_back/src/main/java/com/project/daerkoob/domain/Transcription.java
+++ daerkoob_back/src/main/java/com/project/daerkoob/domain/Transcription.java
@@ -3,12 +3,23 @@ package com.project.daerkoob.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Table(name = "transcription")
 public class Transcription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "transcription_id")
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "book_id")
+    private Long bookId;
+    @Column(name = "thumb")
+    private Long thumb;
+    @Column(name = "star")
+    private Double star;
 }
