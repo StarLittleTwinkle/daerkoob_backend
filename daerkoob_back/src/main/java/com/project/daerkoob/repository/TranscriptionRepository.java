02539cf7210cfec5dcc8488c253c0648e685b415
jaeyeon kim <jakind@naver.com>
2021-11-13 08:11:00 +0900

    findByBookId 추가

--- daerkoob_back/src/main/java/com/project/daerkoob/repository/TranscriptionRepository.java
+++ daerkoob_back/src/main/java/com/project/daerkoob/repository/TranscriptionRepository.java
@@ -4,6 +4,9 @@ import com.project.daerkoob.domain.Transcription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TranscriptionRepository extends JpaRepository<Transcription , Long> {
+    List<Transcription> findByBookId(Long bookId);
 }
