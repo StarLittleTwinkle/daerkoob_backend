55a2789758c76c507c979279357563fd1d357183
jaeyeon kim <jakind@naver.com>
2021-11-18 12:52:33 +0900

    database 수정

--- daerkoob.sql
+++ daerkoob.sql
@@ -34,7 +34,7 @@ CREATE TABLE `book` (
   PRIMARY KEY (`book_id`),
   UNIQUE KEY `book_id_UNIQUE` (`book_id`),
   UNIQUE KEY `ibsn_UNIQUE` (`isbn`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (1,'1','1','1','1','1','1','1'),(2,'2','2','2','2','2','2','2'),(3,'슬픔의 해석','리사 슐먼','일므디','20210711','1197031758 9791197031755','naver.com','누구누구');
+INSERT INTO `book` VALUES (1,'1','1','1','1','1','1','1'),(2,'2','2','2','2','2','2','2'),(3,'슬픔의 해석','리사 슐먼','일므디','20210711','1197031758 9791197031755','naver.com','누구누구'),(4,'<b>해리</b> 포터와 마법사의 돌 (<b>해리</b>포터 20주년)','J.K. 롤링','문학수첩','20191119','8983927747 9788983927743','https://bookthumb-phinf.pstatic.net/cover/157/688/15768828.jpg?type=m1&udate=20200109','<b>해리</b> 포터 세대의, <b>해리</b> 포터 세대를 위한, <b>해리</b> 포터 세대에 의한 새 번역!&#x0D;‘21세기 대표 아이콘’에 걸맞은 완성도 높은 작품으로 재탄생하다!&#x0D;&#x0D;1997년 영국에서 출간된 이래 『<b>해리</b> 포터』 시리즈는 지금까지 200개국 이상 80개의 언어로 번역되고 출간되어 5억 부 이상을 판매했다. 국내에서도... ');
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +63,7 @@ CREATE TABLE `friend` (
   UNIQUE KEY `friend_id_UNIQUE` (`id`),
   KEY `user_index` (`user_index`),
   CONSTRAINT `friend_ibfk_1` FOREIGN KEY (`user_index`) REFERENCES `user` (`user_index`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +72,7 @@ CREATE TABLE `friend` (
 
 LOCK TABLES `friend` WRITE;
 /*!40000 ALTER TABLE `friend` DISABLE KEYS */;
-INSERT INTO `friend` VALUES (1,12,'재연사랑',11);
+INSERT INTO `friend` VALUES (1,12,'재연사랑',11),(2,14,'봉봉알러뷰',11);
 /*!40000 ALTER TABLE `friend` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +94,7 @@ CREATE TABLE `hibernate_sequence` (
 
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (2);
+INSERT INTO `hibernate_sequence` VALUES (3);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,6 +113,9 @@ CREATE TABLE `review` (
   `star` double NOT NULL,
   `review_id` int NOT NULL AUTO_INCREMENT,
   `star_count` int NOT NULL,
+  `book_title` varchar(255) NOT NULL,
+  `user_nick_name` varchar(255) NOT NULL,
+  `register_date` datetime NOT NULL,
   PRIMARY KEY (`review_id`),
   UNIQUE KEY `review_id_UNIQUE` (`review_id`),
   KEY `book_id` (`book_id`),
@@ -152,7 +155,7 @@ CREATE TABLE `star` (
   KEY `review_id` (`review_id`),
   CONSTRAINT `star_ibfk_1` FOREIGN KEY (`transcription_id`) REFERENCES `transcription` (`transcription_id`),
   CONSTRAINT `star_ibfk_2` FOREIGN KEY (`review_id`) REFERENCES `review` (`review_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -161,7 +164,6 @@ CREATE TABLE `star` (
 
 LOCK TABLES `star` WRITE;
 /*!40000 ALTER TABLE `star` DISABLE KEYS */;
-INSERT INTO `star` VALUES (3,11,NULL,9,1,5,12),(3,11,NULL,9,2,5,11);
 /*!40000 ALTER TABLE `star` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -185,7 +187,7 @@ CREATE TABLE `thumb` (
   KEY `transcription_id` (`transcription_id`),
   CONSTRAINT `thumb_ibfk_1` FOREIGN KEY (`review_id`) REFERENCES `review` (`review_id`),
   CONSTRAINT `thumb_ibfk_2` FOREIGN KEY (`transcription_id`) REFERENCES `transcription` (`transcription_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,13 +214,16 @@ CREATE TABLE `transcription` (
   `thumb` int NOT NULL,
   `star` double NOT NULL,
   `star_count` int NOT NULL,
+  `user_nick_name` varchar(255) NOT NULL,
+  `book_title` varchar(255) NOT NULL,
+  `register_date` datetime NOT NULL,
   PRIMARY KEY (`transcription_id`),
   UNIQUE KEY `transcription_id_UNIQUE` (`transcription_id`),
   KEY `user_id` (`user_id`),
   KEY `book_id` (`book_id`),
   CONSTRAINT `transcription_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_index`),
   CONSTRAINT `transcription_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -227,7 +232,7 @@ CREATE TABLE `transcription` (
 
 LOCK TABLES `transcription` WRITE;
 /*!40000 ALTER TABLE `transcription` DISABLE KEYS */;
-INSERT INTO `transcription` VALUES (1,11,1,'나는 바보다',0,0,0),(1,11,3,'1',0,0,0),(2,11,8,'2',0,0,0),(3,11,9,'나는 바보다',0,5,2);
+INSERT INTO `transcription` VALUES (2,11,11,'2',0,0,0,'수희사랑','2','2021-11-18 00:00:00');
 /*!40000 ALTER TABLE `transcription` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -245,12 +250,13 @@ CREATE TABLE `user` (
   `nick_name` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `birth` datetime NOT NULL,
-  `admin` varchar(45) DEFAULT NULL,
   `friend` int NOT NULL,
+  `review_count` int NOT NULL,
+  `transcription_count` int NOT NULL,
   PRIMARY KEY (`user_index`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`),
   UNIQUE KEY `nick_name_UNIQUE` (`nick_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -259,7 +265,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (11,'kpeel5839','김재연','수희사랑','sksk5839!','2021-11-15 15:00:00',NULL,1),(12,'suheelove','황수희','재연사랑','sksk5839!','2021-11-15 15:00:00',NULL,0),(13,'hello','김김김','숲세권주민','1234','2021-11-16 15:00:00',NULL,0);
+INSERT INTO `user` VALUES (11,'kpeel5839','김재연','수희사랑','sksk5839!','2021-11-15 15:00:00',2,0,1),(12,'suheelove','황수희','재연사랑','sksk5839!','2021-11-15 15:00:00',0,0,0),(13,'hello','김김김','숲세권주민','1234','2021-11-16 15:00:00',0,0,0),(14,'suhee831','황수희','봉봉알러뷰','wodustkfkd486','1998-08-28 15:00:00',0,0,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -272,4 +278,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-17 11:33:30
+-- Dump completed on 2021-11-18 12:50:32
