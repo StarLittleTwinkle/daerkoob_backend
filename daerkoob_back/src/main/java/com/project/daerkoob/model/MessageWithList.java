3da55edb5e6c445a12245c7c82ec0a2cbb1f61ba
jaeyeon kim <jakind@naver.com>
2021-12-01 09:54:10 +0900

    model 객체를 모두 MessageWithList로 통합

--- daerkoob_back/src/main/java/com/project/daerkoob/model/MessageWithList.java
+++ daerkoob_back/src/main/java/com/project/daerkoob/model/MessageWithList.java
@@ -1,16 +1,13 @@
 package com.project.daerkoob.model;
 
-import com.project.daerkoob.domain.Review;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import java.util.List;
-
 
 @Data
 @AllArgsConstructor
-public class MessageWithReviewList {
+public class MessageWithList {
     boolean flag;
     String message;
-    List<TransferReview> reviewList;
+    List<Object> List;
 }
