6c576f6f4d0922855d721b60b30ea7ee3cc1ee7c
jaeyeon kim <jakind@naver.com>
2021-12-10 16:14:55 +0900

    best 최종 최종 수정

--- daerkoob_back/src/main/java/com/project/daerkoob/service/BookService.java
+++ daerkoob_back/src/main/java/com/project/daerkoob/service/BookService.java
@@ -51,7 +51,7 @@ public class BookService {
             return bookRepository.findTop10ByOrderByTranscriptionCountDesc();
         }
         else{
-            return bookRepository.findTop10ByOrderByTranscriptionCountDesc();
+            return bookRepository.findTop18ByOrderByTranscriptionCountDesc();
         }
     }
 
